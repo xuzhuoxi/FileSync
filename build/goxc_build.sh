d380c51ab3167880dc2b7ec7f991c33063a7aa9e
xuzhuoxi <xuzhuoxi@gmail.com>
2022-05-07 14:21:41 +0800

    1.更新打包脚本，使用新的verson=1.0.3

--- build/goxc_build.sh
+++ build/goxc_build.sh
@@ -2,4 +2,4 @@
 
 CRTDIR=$(cd `dirname $0`; pwd)
 
-goxc -os="linux darwin windows freebsd openbsd" -arch="amd64 arm" -n=FileSync -pv=v1.0.3 -wd=${CRTDIR}/../src -d=${CRTDIR}/./release -include=*.go,README*,LICENSE*
+goxc -os="linux darwin windows freebsd openbsd" -arch="amd64 arm" -n=FileSync -pv=v1.0.4 -wd=${CRTDIR}/../src -d=${CRTDIR}/./release -include=*.go,README*,LICENSE*
